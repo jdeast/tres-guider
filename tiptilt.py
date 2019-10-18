f1ded6f1134dcc82e190bdea5ab1de8fcf3c80c2
Jason Eastman <jason.eastman@cfa.harvard.edu>
2019-10-18 11:35:26 -0400

    now working example that controls tip/tilt

--- tiptilt.py
+++ tiptilt.py
@@ -1,5 +1,5 @@
 from configobj import ConfigObj
-from pipython import GCSDevice
+from pipython import GCSDevice, pitools
 import socket
 import math
 import ipdb
@@ -17,11 +17,11 @@ class tiptilt:
         self.sntiptilt = config['SN_TIPTILT']
         self.sncontroller = config['SN_CONTROLLER']
 
-        # stage steps per arcsecond on the sky
-        self.steps_per_arcsec = config['STEPS_PER_ARCSEC']
+        # stage steps per arcsecond on the sky 
+        self.steps_per_arcsec = float(config['STEPS_PER_ARCSEC'])
         
         # angle between North and axis A, in radians
-        self.theta = config['THETA']*math.pi()/180.0
+        self.theta = float(config['THETA'])*math.pi/180.0
 
         # if there's a flip in the mapping, apply it
         if config['FLIP']: self.sign = -1.0 
@@ -36,11 +36,29 @@ class tiptilt:
         self.tiptilt.qPOS() # current position
 
     def connect(self):
-        self.tiptilt.EnumerateUSB()
+        usbdevices = self.tiptilt.EnumerateUSB()
+
+
+        #"*** GCSError: There is no interface or DLL handle with the given ID (-9)" That error requires a power cycle
+
+        found = False
+        for device in usbdevices:
+		if self.sncontroller in device:
+			found = True
+        if not found:
+		print 'Serial number supplied for controller (' + self.sncontroller + ') does not match any of the connected USB devices; check ' + self.config_file
+		print "The connected devices are:"
+		print usbdevices
+		return False
+
+		
+
         self.tiptilt.ConnectUSB(serialnum=self.sncontroller)
         #self.tiptilt.ConnectUSB(serialnum=self.sntiptilt)
-        if !self.tiptilt.IsConnected():
+        if not self.tiptilt.IsConnected():
             print 'Error connecting to device'
+
+        pitools.startup(self.tiptilt)
         
     '''
     Move the tip/tilt stage directly in stage coordinates. Check for
@@ -48,18 +66,18 @@ class tiptilt:
     '''
     def move_tip_tilt(self, tip, tilt):
         # make sure we're connected
-        if !self.tiptilt.IsConnected():
+        if not self.tiptilt.IsConnected():
             self.connect()
 
         # TODO: check bounds of requested move
 
         # move
-        self.tiptilt.MOV('A',north)
-        self.tiptilt.MOV('B',east)
+        self.tiptilt.MOV('A',tip)
+        self.tiptilt.MOV('B',tilt)
 
         # make sure it moved where we wanted
-        if position != self.get_position():
-            pass
+        #if position != self.get_position():
+        #    pass
 
         return
 
@@ -72,8 +90,8 @@ class tiptilt:
         # translate north & east (arcsec on sky) to 
         # tip and tilt (stage steps)
         # requires angle, magnitude, and flip
-        tip  = self.sign*self.steps_per_arcsec*(north*math.cos(self.theta) - east*math.sin(theta))
-        tilt =           self.steps_per_arcsec*(north*math.sin(self.theta) + east*math.cos(theta))
+        tip  = self.sign*self.steps_per_arcsec*(north*math.cos(self.theta) - east*math.sin(self.theta))
+        tilt =           self.steps_per_arcsec*(north*math.sin(self.theta) + east*math.cos(self.theta))
 
         # move the tip/tilt
         return self.move_tip_tilt(tip,tilt)
@@ -84,13 +102,17 @@ class tiptilt:
 
 if __name__ == '__main__':
 
-    if socket.gethostname() = 'onion':
+    if socket.gethostname() == 'onion':
         base_directory = '/h/onion0/tres/'
     else:
         base_directory = 'C:/tres-guider/'
     
     config_file = 'tiptilt.ini'
     tiptilt = tiptilt(base_directory, config_file)
+    tiptilt.connect()
+    tiptilt.move_tip_tilt(0.2,0.2)
+
+    
 
     ipdb.set_trace()
 
