f742d2dc11f0800c2710a33f5d8ea8706d3b7214
Jason Eastman <jdeast@gmail.com>
2020-11-23 10:14:26 -0500

    add object for DFM telescope

---
+++ telescope.py
@@ -0,0 +1,258 @@
+# Implementation of TCS from DFM here (pgs 17-32):
+# https://arm.dfmsupport.com/jobs/857/TCSGO%20Manual%20v1.3.pdf
+# local copy here in TCSGO_Manual_v1.3.pdf
+
+from configobj import ConfigObj
+from pipython import GCSDevice, pitools
+import socket
+import math
+import ipdb
+import sys, os
+import utils
+import logging
+import time
+import socket
+
+
+''' 
+this is a telescope class that implements DFM Galil TCS
+'''
+class telescope:
+
+    def __init__(self, base_directory, config_file, logger=None, simulate=False):
+        self.base_directory=base_directory
+        self.config_file = self.base_directory + '/config/' + config_file
+
+        # set up the log file
+        if logger == None:
+            self.logger = utils.setup_logger(base_directory + '/log/', 'telescope')
+        else: self.logger = logger
+
+        # read the config file
+        config = ConfigObj(self.config_file)
+
+        # read the config file
+        if os.path.exists(self.config_file):
+            config = ConfigObj(self.config_file)
+        else:
+            self.logger.error('Config file not found: (' + self.config_file + ')')
+            sys.exit()
+
+        self.server = config['SERVER']
+        self.port = config['PORT']
+        self.epoch = config['EPOCH']
+
+    # send a command to the Galil TCS
+    def send(self,cmd, timeout=5):
+        s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+
+        try:
+            s.connect((self.server,self.port))
+            self.logger.info("Connected")
+        except:
+            self.logger.error("Error connecting to " + self.server + ':' + str(self.port))
+            return None
+            
+        self.logger.info("Sending command: " + cmd)
+        s.send(cmd +'\r')
+
+        data = ''
+        t0 = datetime.datetime.utcnow()
+        time_elapsed = 0.0
+        while True and time_elapsed < timeout:
+            time_elapsed = (datetime.datetime.utcnow()-t0).total_seconds()
+            data += s.recv(1024)
+            if data[-1] == ';': break
+
+        self.logger.info("Closing connection")
+        s.close()
+            
+        if time_elapsed > timeout:
+            self.logger.error("Timeout sending command to TCS Galil")
+
+        self.logger.info("Message received: " + data)
+            
+        return data
+
+    def slew(self, raj2000, decj2000):
+
+        if self.simulate:
+            time.sleep(1.0)
+            return True
+        else:
+            # make sure we're connected
+            if not self.IsConnected():
+                self.connect()
+                if not self.IsConnected():
+                    self.logger.error("Error connecting to telescope")
+                    return False
+
+        # make sure the move is in range
+        if not self.allowedMove(raj2000, decj2000):
+            self.logger.error("Requested move out of bounds")
+            return False
+           
+        # slew the telescope
+        self.logger.info("moving the telescope to RA (J2000)" + str(raj2000) + ', DEC (J2000):' + str(decj2000))
+        if self.simulate:
+            time.sleep(1.0)
+        else: self.send("#6," + str(raj2000) + "," + str(decj2000) + "," + str(epoch) + ";")
+        return True
+
+    # this jogs the telescope. Note if it is not in CHASE mode, set_mount_mode must be set to 1(?)
+    # if requested jog is out of range, status['target_out_of_range'] will be True
+    def offset_target_object(self,east,north):
+
+        # this is a limit imposed by me here. No such limit exists in the telescope
+        if math.abs(east) > self.max_jog or math.abs(north) > self.max_jog:
+            self.logger.error('Cannot jog more than ' + str(self.max_jog) + '" in any direction')
+        
+        self.send("#7," + str(east) + "," + str(north) + ";")
+
+    
+    def set_mount_mode(self, mode):
+        if mode <> 0 and mode <> 1 and mode <> 2 and mode <> 3:
+            self.logger.error("Supplied mount mode (" + str(mode) + ") not allowed; must be 0 (Hold Position), 1 (Follow Target), 2 (Slew to Target), or 3 (Chase Target)")
+            return False
+            
+        return self.send("#12," + str(mode) + ";")
+
+    def read_mount_coordinates(self):
+        response = self.send("#25;")
+
+        # trim leading "#" and trailing ";", then split by ','
+        arr = response[1:-1].split(',')
+        coords = {}
+        if len(arr) == 10:
+            # convert date and time to a datetime object
+            # what is a 'decimal year'? It doesn't change often enough to be precise,
+            # nor is it an integer number of days...
+            year = int(arr[9]) 
+            seconds = (float(arr[9])-year)*365.25*86400.0
+            hours = float(arr[8])
+            date = datetime.datetime(year,1,1,0) + datetime.timedelta(seconds=seconds)
+            
+            coords['HA'] = arr[0]
+            coords['RA'] = arr[1]
+            coords['Dec'] = arr[2]
+            coords['equinox'] = arr[3]
+            coords['airmass'] = arr[4]
+            coords['zenith_distance'] = arr[5]
+            coords['azimuth'] = arr[6]
+            coords['sidereal_time'] = arr[7]
+            coords['date'] = date
+        return coords
+    
+    def read_tcs_status(self):
+        response = self.send("#26;")
+
+        # trim leading "#" and trailing ";", then split by ','
+        arr = response[1:-1].split(',')
+
+        status = {}
+        if len(arr) == 8:
+            byte0 = format(int(arr[0]),'b').zfill(8)
+            status['drive_switch_up']      = byte0[0]=='1'
+            status['track_switch_up']      = byte0[1]=='1'
+            status['aux_track_switch_up']  = byte0[2]=='1'
+            status['dome_track_switch_up'] = byte0[3]=='1'
+            status['auto_dome_switch_up']  = byte0[4]=='1'
+            status['amplifiers_switch_up'] = byte0[5]=='1'
+
+            byte1 = format(int(arr[1]),'b').zfill(8)
+            status['north_button_pressed'] = byte1[0]=='1'
+            status['south_button_pressed'] = byte1[1]=='1'
+            status['east_button_pressed']  = byte1[2]=='1'
+            status['west_button_pressed']  = byte1[3]=='1'
+            status['set_button_pressed']   = byte1[4]=='1'
+            status['slew_button_pressed']  = byte1[5]=='1'
+            status['ascom_guiding']        = byte1[6]=='1'
+
+            byte2 = format(int(arr[2]),'b').zfill(8)
+            status['horizon_limit_reached']  = byte2[0]=='1'
+            status['east_travel_limit']      = byte2[1]=='1'
+            status['west_travel_limit']      = byte2[2]=='1'
+            status['north_travel_limit']     = byte2[3]=='1'
+            status['south_travel_limit']     = byte2[4]=='1'
+            status['target_out_of_range']    = byte2[5]=='1'
+            status['approaching_soft_limit'] = byte2[6]=='1'
+            status['reached_soft_limit']     = byte2[7]=='1'
+
+            byte3 = format(int(arr[3]),'b').zfill(8)
+            status['find_home_mode'] = byte3[0]=='1'
+            status['goto_az_mode']   = byte3[1]=='1'
+            status['track_mode']     = byte3[2]=='1'
+            status['rotating_left']  = byte3[3]=='1'
+            status['rotating_right'] = byte3[4]=='1'
+            status['at_home']        = byte3[5]=='1'
+            status['manual_mode']    = byte3[6]=='1'
+
+            byte4 = format(int(arr[4]),'b').zfill(8)
+            status['focus_moving_in']        = byte4[0]=='1'
+            status['focus_moving_out']       = byte4[1]=='1'
+            status['focus_at_outer_limit']   = byte4[2]=='1'
+            status['focus_at_inner_limit']   = byte4[3]=='1'
+            status['instrument_rotator_ccw'] = byte4[4]=='1'
+            status['instrument_rotator_cw']  = byte4[5]=='1'
+
+            byte5 = format(int(arr[5]),'b').zfill(8)
+            status['serial_comm_ready']        = byte5[0]=='1'
+            status['tcpip_connected']          = byte5[1]=='1'
+            status['use_cos_dec']              = byte5[2]=='1'
+            status['use_rate_corrections']     = byte5[3]=='1'
+            status['east_side_pier_operation'] = byte5[4]=='1'
+            status['west_side_pier_operation'] = byte5[5]=='1'
+            status['galil_data_is_stale']      = byte5[6]=='1'
+
+            byte6 = format(int(arr[6]),'b').zfill(8)
+            status['mount_in_chase_target_mode']   = byte6[0]=='1'
+            status['mount_in_slew_to_target_mode'] = byte6[1]=='1'
+            status['mount_in_follow_target_mode']  = byte6[2]=='1'
+            status['mount_in_hold_position_mode']  = byte6[3]=='1'
+            status['target_captured']              = byte6[4]=='1'
+            
+            byte7 = format(int(arr[7]),'b').zfill(8)
+            status['dome_upper_shutter_open']   = byte7[0]=='1'
+            status['dome_upper_shutter_closed'] = byte7[1]=='1'
+            status['dome_lower_shutter_open']   = byte7[2]=='1'
+            status['dome_lower_shutter_closed'] = byte7[3]=='1'
+            status['mirror_doors_open']         = byte7[4]=='1'
+            status['mirror_doors_closed']       = byte7[5]=='1'
+
+        return status
+            
+    # turns on the pointing model corrections
+    def apply_mount_corrections(self):
+        self.logger.info("Turning on mount corrections")
+        self.send('#66,1;')
+
+    # turns off the pointing model corrections
+    def unapply_mount_corrections(self):
+        self.logger.info("Turning off mount corrections")
+        self.send('#66,0;')
+
+
+    
+if __name__ == '__main__':
+
+    if socket.gethostname() == 'tres-guider':
+        base_directory = '/home/tres/tres-guider/'
+    elif socket.gethostname() == 'Jason-THINK':
+        base_directory = 'C:/tres-guider/'
+    elif socket.gethostname() == 'flwo60':
+        base_directory = '/home/observer/tres/'
+    else:
+        print('unsupported system')
+        sys.exit()
+
+    config_file = 'telescope.ini'
+    telescope = telescope(base_directory, config_file)
+    
+    status = telscope.read_tcs_status()
+    if status['target_out_of_range']:
+        print('target out of range')
+
+        
+        
+    ipdb.set_trace()
+
